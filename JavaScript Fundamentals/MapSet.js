b48cabc86f9ab6292a15d18fe20cadc5cb0a809d
EYE MOTION <eyemotiongamestudio@gmail.com>
2022-02-20 20:33:06 +0400

    update

---
+++ JavaScript Fundamentals/MapSet.js
@@ -0,0 +1,12 @@
+// Map and Set
+
+// Object is a keyed collection
+// Array is a orderd collection
+
+// Map is a collection of keyed data items, just like a Object(any type)
+
+// Methods 
+// new Map() : this one is creating new map
+// map.set(key, value) this method is store the value by the key.
+// map.get(key) this method returns the value by the key and it returns undefined if key doesnot exists in map
+//  
